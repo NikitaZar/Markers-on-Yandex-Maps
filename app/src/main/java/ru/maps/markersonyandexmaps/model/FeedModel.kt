1c00fcede0afc3a40264d01eb0d19b255aef3778
NikitaZar <webbizon@ya.ru>
2022-04-28 12:05:50 +0400

    ViewModel level done

---
+++ app/src/main/java/ru/maps/markersonyandexmaps/model/FeedModel.kt
@@ -0,0 +1,9 @@
+package ru.maps.markersonyandexmaps.model
+
+import ru.maps.markersonyandexmaps.dto.Marker
+
+data class FeedModel(
+    val markers: List<Marker> = emptyList(),
+) {
+    val empty = markers.isEmpty()
+}
---
+++ app/src/main/java/ru/maps/markersonyandexmaps/viewModel/MarkerViewModel.kt
@@ -0,0 +1,55 @@
+package ru.maps.markersonyandexmaps.viewModel
+
+import androidx.lifecycle.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import ru.maps.markersonyandexmaps.dto.Marker
+import ru.maps.markersonyandexmaps.model.FeedModel
+import ru.maps.markersonyandexmaps.repository.MarkerRepository
+import ru.maps.markersonyandexmaps.util.SingleLiveEvent
+import javax.inject.Inject
+
+private val empty = Marker(
+    id = 0,
+    description = ""
+)
+
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@HiltViewModel
+class MarkerViewModel @Inject constructor(
+    private val repository: MarkerRepository
+) : ViewModel() {
+
+    val data: LiveData<FeedModel> =
+        repository.data.map {
+            FeedModel(it)
+        }.asLiveData(Dispatchers.Default)
+
+    private val edited = MutableLiveData(empty)
+
+    private val _postCreated = SingleLiveEvent<Unit>()
+
+    val postCreated: LiveData<Unit>
+        get() = _postCreated
+
+
+    fun save() = viewModelScope.launch {
+        edited.value?.let { repository.save(it) }
+    }
+
+    fun edit(marker: Marker) {
+        edited.value = marker
+    }
+
+    fun remove(id: Long) = viewModelScope.launch {
+        repository.remove(id)
+    }
+
+    fun getMarker(id: Long) = viewModelScope.launch {
+        repository.getMarker(id)
+    }
+}
