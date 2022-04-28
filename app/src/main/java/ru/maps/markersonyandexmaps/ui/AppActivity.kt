b821fc521140329aa22a2dd9a553d6f1ea4f95d9
NikitaZar <webbizon@ya.ru>
2022-04-28 22:55:25 +0400

    Issue in binding.bottomNavigation.setOnItemReselectedListener

--- app/src/main/java/ru/maps/markersonyandexmaps/ui/AppActivity.kt
+++ app/src/main/java/ru/maps/markersonyandexmaps/ui/AppActivity.kt
@@ -2,10 +2,8 @@ package ru.maps.markersonyandexmaps.ui
 
 import android.os.Bundle
 import android.util.Log
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import com.google.android.material.navigation.NavigationBarView
 import dagger.hilt.android.AndroidEntryPoint
 import ru.maps.markersonyandexmaps.R
 import ru.maps.markersonyandexmaps.databinding.ActivityAppBinding
@@ -19,15 +17,13 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
         val binding = ActivityAppBinding.inflate(layoutInflater)
 
         binding.bottomNavigation.setOnItemReselectedListener { item ->
-            Log.i("item", "${item.itemId}")
+            Log.i("navigate", "${item.itemId}")
             val fragmentId = when (item.itemId) {
                 R.id.page_list -> R.id.listFragment
                 R.id.page_map -> R.id.mapFragment
                 else -> null
             }
-            Log.i("item", "$fragmentId")
             fragmentId?.let { findNavController(R.id.nav_host_fragment).navigate(it) }
-            true
         }
     }
 }
